c4cfea707a531b84e9d56d8162fe9258ca54ee68
modpage1512 <65136626+modpage1512@users.noreply.github.com>
2021-09-26 18:47:56 -0400

    Update main.js

--- main.js
+++ main.js
@@ -237,7 +237,7 @@ const idiomChangeAbout = (language) => {
         document.querySelector(".personal-title").innerHTML = 'Información <span class="text-secondary">Personal</span>';
         document.querySelector(".personal").innerHTML = '<p>Mi nombre es Carlos Castillo, soy de Venezuela, estado La Guaria.<br><br>' +
         'Tengo 24 años y soy programador web, actualmente me enfoco en el área del front-end del desarrollo web.<br><br> Soy una persona competitiva, resiliente y apasionado de la tecnología, siempre busco mejorar y aprender constantemente.</p>' +
-        '<h4><span class="text-secondary"><a href="archivos/Cv-qr.pdf" download="https://github.com/modpage1512/modpage1512.github.io/blob/main/Archivos/Cv-qr.pdf">Descargar curriculum</a></span></h4>';
+        '<h4><span class="text-secondary"><a href="https://drive.google.com/drive/u/0/my-drive" download">Descargar curriculum</a></span></h4>';
         document.querySelector(".job-1 h2").innerHTML = "Aptitudes";
         document.querySelector(".job-2 h2").innerHTML = "Herramientas";
         document.querySelector(".job-3 h2").innerHTML = "Redes sociales";
